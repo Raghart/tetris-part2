396cb499efb543a6a6ccbd97ce46278fa75adcd8
Rafael <rafaelrojas36@outlook.com>
2025-08-23 18:31:19 -0400

    block Error handling

--- src/Board.mjs
+++ src/Board.mjs
@@ -3,20 +3,26 @@ export class Board {
   height;
   grid; 
   pos;
+  block;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.grid = Array.from({ length: width }, () => Array.from({ length: height }, () => "."));
     this.pos = { x: 0, y: Math.floor(this.width / 2) };
+    this.block = "";
   }
 
   tick() { 
     this.grid = this.grid.map(arr => arr.map(() => "."));
-    this.grid[++this.pos.x][this.pos.y] = "X"
+    this.grid[++this.pos.x][this.pos.y] = this.block;
   }
 
-  drop() { this.grid[this.pos.x][this.pos.y] = "X" }
+  drop(block) {
+    if (this.block && this.block !== block) throw new Error("already falling");
+    this.block = block;
+    this.grid[this.pos.x][this.pos.y] = block;
+  }
 
   toString() { return this.grid.map(row => row.join("") + "\n").join(""); }
 }
