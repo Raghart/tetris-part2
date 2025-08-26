e23c5c9b5dcc13a17714e0eb004bdc5b55bd153a
Rafael <rafaelrojas36@outlook.com>
2025-08-26 16:55:19 -0400

    Implemeted rotate right

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,3 +1,5 @@
+import { reverse } from "lodash";
+
 export class RotatingShape {
     constructor (matrix) {
         this.matrix = matrix;
@@ -6,6 +8,12 @@ export class RotatingShape {
     static fromString(str) {
         const rows = str.split("\n").map(row => row.trim().split(""));
         return new RotatingShape(rows);
+    }
+
+    rotateRight() {
+        const rotatedMatrix = [];
+        for (let i = 0; i < this.matrix.length; i++) { rotatedMatrix.push(this.matrix.map(row => row[i])); }
+        return new RotatingShape(rotatedMatrix.map(row => [...row].reverse()));
     }
 
     toString() {
