11c22cc5d64dc6f0cd5924e188104f441db09013
Rafael <rafaelrojas36@outlook.com>
2025-08-29 11:54:21 -0400

    Fixing logic in new Board function

--- src/Board.mjs
+++ src/Board.mjs
@@ -50,12 +50,13 @@ export class Board {
 
   hasFalling() { return this.isFalling; }
 
-  drawInBoard(block, position) {
-    for (let row=0; row < this.block.matrix.length; row++) {
-      for (let col=0; col < this.block.matrix[row].length; col++) {
-        if (this.block.matrix[row][col] !== "." && Array.isArray(block)) { 
+  drawBlock(block, position) {
+    const midCol = Math.floor((this.grid[0].length - block[0].length)/2)
+    for (let row=0; row < block.length; row++) {
+      for (let col=0; col < block[row].length; col++) {
+        if (block[row][col] !== ".") { 
           this.grid[position + row][midCol + col] = block[row][col];
-        } else { this.grid[position + row][midCol + col] = "."; }
+        }
       }
     }}
 
